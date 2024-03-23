dd06fd63c8308a9ab46a75f46a77aabcde2bae0c
Ji Park <ji.park@jpnws.com>
2024-03-22 20:09:41 -0700

    Replace infra files with example files.

--- infra/test/chapter-3.test.ts
+++ infra/test/chapter-3.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infra from '../lib/infra-stack';
+// import * as Chapter3 from '../lib/chapter-3-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
+// example resource in lib/chapter-2-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
+//   const stack = new Chapter3.Chapter2Stack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
